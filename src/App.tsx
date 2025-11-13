4494d62e9334274f920bd22e2cdfc0f5ea615461
CDT Ishaan <percyjackson7.riptide@gmail.com>
2025-11-13 19:11:58 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { TournamentProvider } from "@/contexts/TournamentContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
@@ -25,7 +25,7 @@ const App = () => (
           <TournamentProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <HashRouter>
               <Layout>
                 <Routes>
                   <Route path="/" element={<Home />} />
@@ -43,7 +43,7 @@ const App = () => (
                   <Route path="*" element={<NotFound />} />
                 </Routes>
               </Layout>
-            </BrowserRouter>
+            </HashRouter>
           </TournamentProvider>
         </AuthProvider>
       </ThemeProvider>
